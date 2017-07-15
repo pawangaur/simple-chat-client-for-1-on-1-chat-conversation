d79ee433455168c4e4919ac6106349f9867a1f5e
pawangaur <p1god.cse14@nituk.ac.in>
2017-07-15 17:42:30 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,54 @@
+import socket
+import sys
+import select
+from thread import *
+
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
+if len(sys.argv)!=3:
+	print "Correct usage:script,IP address,port number"
+	exit()
+
+IP_address=str(sys.argv[1])
+Port=int(sys.argv[2])
+
+server.bind((IP_address,Port))
+server.listen(100)
+
+list_of_client=[]
+
+def clientthread(conn,addr):
+	conn.send("Welcome to my chatroom!")
+	while True:
+		try:
+			message=conn.recv(2048)
+			if message:
+				print "<"+addr[0]+">"+message
+				message_to_send="<"+addr[0]+">"+message
+				broadcast(message_to_send,conn)
+			else:
+				remove(conn)
+		except:
+			continue
+
+def broadcast(message,conn):
+	for clients in list_of_client:
+		if clients!=conn:
+			try:
+				clients.send(message)
+			except:
+				clients.close()
+				remove(clients)
+
+def remove(conn):
+	if conn in list_of_client:
+		list_of_client.remove(conn)
+
+while True:
+	conn ,addr=server.accept()
+	list_of_client.append(conn)
+	print addr[0]+" connected"
+	start_new_thread(clientthread,(conn,addr))
+conn.close()
+server.close()
